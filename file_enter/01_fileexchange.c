9044ca2ba31f067b5226c93e9fa9bc39abb8ac27
sinn <rWKN.PTA1078@gmail.com>
2018-05-31 17:59:04 +0900

    とあるファイルとそのデータの行列入れ替え操作のための勉強用(作成中)

---
+++ file_enter/01_fileexchange.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main ()
+{
+
+    int N;
+    double str_data[4][N];
+    char str[1000];
+    char *tor;
+    FILE *fp;
+    int counter = 1;
+
+    fp = fopen("./00_dat.csv","r");
+    if(fp=NULL)
+    {
+        printf("file is not exist\n");
+        return 1;
+    }
+
+    fscanf(fp,"%s",str);
+    tor = strtok(str, ",");
+    while (tor != NULL)
+    {
+        counter ++;
+        tor = strtok(NULL, ",");
+    }
+
+    // バッファオーバーフロー発生・確認が必要
+
+    printf("%d datas\n",counter);
+
+    fclose(fp);
+
+    return 0;
+}
