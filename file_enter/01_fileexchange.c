a1d8a5562538f6c17504edde780cfc31b0dd1fd6
iwasakisinnya <rwkn.pta1078@gmail.com>
2018-06-13 13:44:10 +0900

    [C/add] strtokによる配列読み込みの追加
    問題点；
    データ格納がうまくいってない。ポインターの問題？文字列が何か悪さしてる？

--- file_enter/01_fileexchange.c
+++ file_enter/01_fileexchange.c
@@ -2,14 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 
-void StoreValue(int row, int I, int J, int N,  FILE *fp, double data[I][J]);
+void StoreValue(FILE *fp, int col, int I, int J, char data_label[10][I], double data[J][I]);
 void MtrxSzCulc(FILE *fp, int *ROWM, int *COLM);
 
 int main()
 {
 
     int N =10000;
-    double str_data[4][N];
     char str[N];
     FILE *fp;
     int counter = 0;
@@ -24,31 +23,37 @@ int main()
 
     MtrxSzCulc(fp, &ROW_MAX, &COL_MAX);
 
+    double str_data[ROW_MAX][COL_MAX];
+    char str_data_label[10][COL_MAX];
+
     printf ("%d %d \n",ROW_MAX,COL_MAX);
 
     fseek(fp,0L,SEEK_SET);
     // ファイルの先頭に移動
 
-    for (size_t row = 0; row < 4; row++)
+    for (size_t col = 0; col < 4; col++)
     {
-        StoreValue(row,4,26,N,fp,str_data);
+        StoreValue(fp, col, 26, 4, str_data_label, str_data);
+        printf("%s \n", str_data_label[col]);
     }
 
-    printf("%lf %lf \n", str_data[1][2], str_data[4][26]);
+    printf("%lf %s \n", str_data[0][2],str_data_label[0]);
 
     fclose(fp);
     return 0;
 }
 
-void StoreValue(int row,int I,int J,int N,FILE *fp,double data[I][J])
+void StoreValue(FILE *fp,int col,int I,int J,char data_label[10][col],double data[J][col])
 {
-    // どのようにしてデータ格納しようか…
+    // strtokを用いたデータ格納の簡略化
     char Temp[1000];
-    fgets(Temp, N, fp);
+    fgets(Temp, sizeof(Temp), fp);
     
+    strcpy(data_label[col], strtok(Temp,","));
     for(size_t c = 0; c < J; c++)
     {
-        sscanf(Temp,"%lf,%[^\n]",&data[I][c],Temp);
+        // sscanf(Temp,"%lf,%[^\n]",&data[col][c],Temp);
+        data[col][c] = atof(strtok(NULL,","));
     }    
 }
 
